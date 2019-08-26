ebf39cf968a2e0bacc7c5a0b31da7310d2a08bfe
Prawared Bowonphattharawadi <prawared.bw@gmail.com>
2019-08-26 15:00:49 +0700

    add generator

--- blocks/blocks_tr-emission.js
+++ blocks/blocks_tr-emission.js
@@ -1,15 +1,22 @@
-Blockly.Blocks['ky_005_tr_emission_block'] = {
-  init: function() {
-    this.appendDummyInput()
-        .appendField("KY-005 TR EMISSION");
-    this.appendValueInput("KY_005_TR_EMISSION_PIN")
-        .setCheck("Number")
-        .appendField("PIN");
-    this.setInputsInline(true);
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
-    this.setColour(230);
- this.setTooltip("");
- this.setHelpUrl("");
-  }
-};
+Blockly.Blocks["ky_005_tr_emission_block"] = {
+    init: function() {
+      this.appendDummyInput().appendField("KY-005 TR EMISSION");
+      this.appendDummyInput()
+        .appendField("CODE")
+        .appendField(
+          new Blockly.FieldTextInput("0xa90"),
+          "KY_005_TR_EMISSION_CODE"
+        )
+        .appendField("VALUE")
+        .appendField(
+          new Blockly.FieldTextInput("12"),
+          "KY_005_TR_EMISSION_VALUE"
+        );
+      this.setInputsInline(true);
+      this.setPreviousStatement(true, null);
+      this.setNextStatement(true, null);
+      this.setColour(230);
+      this.setTooltip("");
+      this.setHelpUrl("");
+    }
+  };
--- blocks/generators_tr-emission.js
+++ blocks/generators_tr-emission.js
@@ -1,6 +1,18 @@
-Blockly.JavaScript['ky_005_tr_emission_block'] = function(block) {
-  var value_ky_005_tr_emission_pin = Blockly.JavaScript.valueToCode(block, 'KY_005_TR_EMISSION_PIN', Blockly.JavaScript.ORDER_ATOMIC);
-  // TODO: Assemble JavaScript into code variable.
-  var code = '...;\n';
-  return code;
-};
+Blockly.JavaScript["ky_005_tr_emission_block"] = function(block) {
+    var text_ky_005_tr_emission_code = block.getFieldValue(
+      "KY_005_TR_EMISSION_CODE"
+    );
+    var text_ky_005_tr_emission_value = block.getFieldValue(
+      "KY_005_TR_EMISSION_VALUE"
+    );
+    // TODO: Assemble JavaScript into code variable.
+    var code = `
+    #EXTINC
+	#include <IRremote.h>
+	IRsend irsend;
+	#END
+
+	irsend.sendSony(${text_ky_005_tr_emission_code}, ${text_ky_005_tr_emission_value}); // Sony TV power code
+    `;
+    return code;
+  };
